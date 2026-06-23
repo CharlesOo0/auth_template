cab8e18e8180fd79edac0afa9049c912e793a82f
Charles <didi831313@gmail.com>
2026-06-23 21:25:15 +0200

    Setup backend dependencies and configurations

---
+++ authentication/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+class LoginView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
